c31432dd193e802d5bfa4b687e712672420e05ef
IreneuszKopiec <115220213+IreneuszKopiec@users.noreply.github.com>
2023-04-03 20:31:35 +0200

    update

--- program31.py
+++ program31.py
@@ -1,43 +1,24 @@
 import math
 
-list1=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14]
-for i in range(15):
-    a = i ** 5
-    list1[i] = a
+list1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+newlist1 = [x**5 for x in list1]
+print(newlist1)
 
-print(list1)
+list2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
 
-list2=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
-
-
-def silnia(el):
-    wynik = 1
-    if el == 1:
-        return 1
-    for i in range(el):
-        wynik = wynik * (i+1)
-    return wynik
+newlist2 = [math.factorial(x) for x in list2]
 
 
-for i in range(20):
-    list2[i] = silnia(i)
-
-
-print(list2)
+print(newlist2)
 
 list3 = []
 for i in range(20):
-    if i == 0:
-        list3.append(1)
-    else:
-        list3.append(math.e**i)
+    list3.append(i)
 
-print(list3)
+newlist3 = [math.e**x for x in list3]
+print(newlist3)
 
-list4 = ["Adam","Maciek","Jan"]
-list5 = []
-
-for i in range(3):
-    list5.append(len(list4[i]))
+list4 = ["Adam", "Maciek", "Jan"]
+list5 = [len(x) for x in list4]
 
 print(list5)
